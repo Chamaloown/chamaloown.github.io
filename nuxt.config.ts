5db37e2ba6580c19c29732a45772c717dcd557f8
Landemaine Malo <malo.landemaine@gmail.com>
2024-10-29 21:52:13 +0100

    feat(ops): github workfow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
